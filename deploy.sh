c0f8942c7ccb2bdcf90715b4de5e21abffa8fb72
tiannaru <piersinaru.cristian@gmail.com>
2021-07-24 21:14:16 +0300

    misc changes; i just need to push

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,21 @@ BACKUP_DIR="$HOME/.dotfiles_backup"
 DOTFILES_URL="http://github.com/tiannaru/dotfiles.git"
 DOTFILES_LOCATION="$HOME/.dotfiles"
 # Ensure that we have the dotfiles repo cloned in the proper location
-if ![test -d $HOME/.dotfiles]; then
+if ! test -d "$HOME/.dotfiles"; then
   git clone DOTFILES_URL DOTFILES_LOCATION
 fi
-cd $DOTFILES_LOCATION
+cd "$DOTFILES_LOCATION" || exit
 
 # Optionally, do a backup. This is nice if people don't want to commit.
 # TODO also the a recovery script
-# if [ $arg ] && [ $arg = "--backup" ]; then
-#   echo -e "Also doing a backup inside $BACKUP_DIR\n"
-#   mkdir $BACKUP_DIR
-#   for dir in $(ls -d */); then
-#     mv ~/$dir ~/.$BACKUP_DIR/$dir
-#   done
-# fi
+if [ $arg ] && [ $arg = "--backup" ]; then
+    echo -e "Backup not implemented yet 😅"
+  # echo -e "Also doing a backup inside $BACKUP_DIR\n"
+  # mkdir $BACKUP_DIR
+  # for dir in $(ls -d */); then
+  #   mv ~/$dir ~/.$BACKUP_DIR/$dir
+  # done
+fi
 
 # Instead of mucking around with the gitconfig file, I'd rather have it source
 # a gitconfig file I have in git, and let the default ~/.gitconfig file loose
@@ -29,7 +30,7 @@ else
   cat >> ~/.gitconfig <<- EOM
   [include]
   path = ~/.config/extra.gitconfig
-  EOM
+EOM
 fi
 
-stow */
+stow "*/"
