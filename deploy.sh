bcf74125c511bacca0bf3901646553f0de501ba6
tiannaru <tiannaru42@gmail.com>
2022-01-23 13:28:06 +0200

    add gitconfig logic to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
 stow $(ls -d */ | grep -v ansible)
+# Instead of mucking around with the gitconfig file, I'd rather have it source
+# a gitconfig file I have in git, and let the default ~/.gitconfig file loose
+if grep -q "extra.gitconfig" ~/.gitconfig; then
+  echo ".gitconfig properly configured already...weird"
+else
+  cat >> ~/.gitconfig <<- EOM
+  [include]
+  path = ~/.config/extra.gitconfig
+EOM
+fi
+
