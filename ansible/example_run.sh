4cbfb08ec29d27c71d928af4b3b22bb14b50fafd
tiannaru <tiannaru42@gmail.com>
2022-01-23 16:09:15 +0200

    harcode the example run for now

--- ansible/example_run.sh
+++ ansible/example_run.sh
@@ -1,3 +1,3 @@
 /bin/bash
 
-ansible localhost -m include_role -a name=dotfiles --extra-vars "user=example github_email=example@example.com github_name=example" --ask-become-pass
+ansible localhost -m include_role -a name=dotfiles --extra-vars "user=cris github_email=tiannaru42@gmail.com github_name=tiannaru" --ask-become-pass
