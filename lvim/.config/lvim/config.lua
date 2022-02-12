e7122c3f78c40e588889f0ff1f7cc41a8e1b8406
tiannaru <tiannaru42@gmail.com>
2022-02-12 14:12:48 +0200

    config null-ls for python and luna

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.builtin.bufferline.active = true
 lvim.builtin.bufferline.config = {
-  icons= "both",
+	icons = "both",
 }
 vim.opt.clipboard = ""
 lvim.transparent_window = true
@@ -109,61 +109,33 @@ lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"css",
+	"rust",
+	"java",
+	"yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ exe = "black" },
+	{ exe = "stylua" },
+})
 
--- set a formatter if you want to override the default lsp one (if it exists)
--- lvim.lang.python.formatters = {
---   {
---     exe = "black",
---   }
--- }
--- set an additional linter
--- lvim.lang.python.linters = {
---   {
---     exe = "flake8",
---   }
--- }
-
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{ exe = "pylint", args = { "-E" } },
+	{ exe = "luacheck", args = { "-g" } },
+})
 -- Additional Plugins
 -- lvim.plugins = {
 --     {"folke/tokyonight.nvim"},
@@ -173,8 +145,8 @@ lvim.builtin.treesitter.highlight.enabled = true
 --     },
 -- }
 lvim.plugins = {
-  { "rktjmp/lush.nvim" },
-  { "ellisonleao/gruvbox.nvim" },
+	{ "rktjmp/lush.nvim" },
+	{ "ellisonleao/gruvbox.nvim" },
 	{ "christianchiarulli/nvcode-color-schemes.vim" },
 	{ "bkad/CamelCaseMotion" },
 	{ "pearofducks/ansible-vim" },
@@ -188,9 +160,9 @@ lvim.plugins = {
 	{ "saltstack/salt-vim" },
 	{ "mfussenegger/nvim-dap-python" },
 	{ "farmergreg/vim-lastplace" },
-  { "mbbill/undotree" },
-  { "nvim-telescope/telescope-symbols.nvim" },
-  { "ojroques/vim-oscyank" },
+	{ "mbbill/undotree" },
+	{ "nvim-telescope/telescope-symbols.nvim" },
+	{ "ojroques/vim-oscyank" },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
