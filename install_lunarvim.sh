2a692fe5491f9cc616fb072765a1d532fd81c8e9
crpier <crpier42@gmail.com>
2023-08-04 20:41:24 +0300

    remove stuff I don't use anymore

--- install_lunarvim.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This is a low effort workaround for until there is a pacman package package for lunarvim (or at least a yay pagacke that works for me lmao)
-LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
